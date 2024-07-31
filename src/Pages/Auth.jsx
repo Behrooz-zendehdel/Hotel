34ee38c7d4436ef1cc9a634eb48efe288a614e96
behrooz <93223889+Behrooz-zendehdel@users.noreply.github.com>
2024-07-31 19:32:47 +0330

    add logic checkotpform

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import SendOTPForm from "../featuers/Authentication/SendOTPForm";
-
+import CheckOTPForm from "../featuers/Authentication/CheckOTPForm";
 function Auth() {
   return (
+    <div className="flex justify-center pt-10">
+
     <div className="w-full sm:max-w-sm">
       <SendOTPForm />
+      <CheckOTPForm />
+    </div>
     </div>
   );
 }
