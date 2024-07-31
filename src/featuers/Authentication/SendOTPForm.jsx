17e8441dd622aca968dd75c3743d668817764570
behrooz <93223889+Behrooz-zendehdel@users.noreply.github.com>
2024-07-31 19:52:26 +0330

    add react-query

--- src/featuers/Authentication/SendOTPForm.jsx
+++ src/featuers/Authentication/SendOTPForm.jsx
@@ -3,16 +3,21 @@ import TextField from "../../Hooks/TextField";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const sendOtpHandler =(e)=>{
+    e.preventDefualt();
+
+
+  }
   return (
     <div className="">
-      <form action="" className="space-y-8">
+      <form action="" className="space-y-8" onSubmit={sendOtpHandler}>
         <TextField
         label="شماره موبایل"
           name="phoneNumber"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button className="btn btn--primary w-full">ارسال کد تایید</button>
+        <button type="submit" className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );
